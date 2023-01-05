f3a25df7efc0bd99cd361886a59edf65c962edca
jjzapata99 <jjzapata@espol.edu.ec>
2023-01-05 16:22:57 -0500

    temperature api last sensed

--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiConectionService} from "../services/api-conection.service"
 
 @Component({
   selector: 'app-temperature',
   templateUrl: './temperature.component.html',
   styleUrls: ['./temperature.component.sass']
 })
-export class TemperatureComponent {
+export class TemperatureComponent implements  OnInit{
+  data : any;
+
+  constructor(private api: ApiConectionService) { }
+
+  ngOnInit(): void {
+    this.getLastTemperature();
+  }
+  getLastTemperature(){
+    this.api.getQuery('getlastSensed').subscribe((response: any) => {
+      this.data=response
+      console.log(this.data)
+
+    });
+  }
+
 
 }
