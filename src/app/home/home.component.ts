309a63e3a30063e4cce0c469f765d6ea23c15c79
Carlos <caencede@espol.edu.ec>
2023-01-05 14:35:48 -0500

    background response, div aligns and flex

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
-
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+declare var anime: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+
   images = ["./assets/img/LRA_photos_1.png", "./assets/img/LRA_photos_2.jpg", "./assets/img/LRA_photos_3.jpg","./assets/img/LRA_photos_4.jpg"];
+
+  ngAfterViewInit(): void {
+    var textWrapper = document.querySelector('.ml11 .letters');
+    textWrapper!.innerHTML = textWrapper!.textContent!.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+      .add({
+        targets: '.ml11 .line',
+        scaleY: [0,1],
+        opacity: [0.5,1],
+        easing: "easeOutExpo",
+        duration: 700
+      })
+      .add({
+        targets: '.ml11 .line',
+        translateX: [0, document!.querySelector('.ml11 .letters')!.getBoundingClientRect().width + 10],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100
+      }).add({
+      targets: '.ml11 .letter',
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=775',
+      delay: (el:any, i:any) => 34 * (i+1)
+    }).add({
+      targets: '.ml11',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 10000000000
+    });
+  }
+
 
 }
